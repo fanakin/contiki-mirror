3aeb3468aa7e44d6bab4e16e325081f1eeb58c60
Fabio Giovagnini <fabio.giovagnini@aurion-tech.com>
2012-02-14 17:45:03 +0100

    implementation working

--- platform/arnNanoM/dev/leds-arch.c
+++ platform/arnNanoM/dev/leds-arch.c
@@ -28,23 +28,40 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: leds-arch.c,v 1.1 2006/06/17 22:41:31 adamdunkels Exp $
+ * $Id: leds-arch.c,v 1.1 2012/02/14 22:41:31 fabiogiovagnini Exp $
  */
 
 /**
  * \file
  *         A brief description of what this file is.
  * \author
- *         Adam Dunkels <adam@sics.se>
+ *         Fabio Giovagnini <fabio.giovagnini@auiron-tech.com>
  */
 
+#include "36064s.h"
 #include "dev/leds.h"
+
+#define LED0				IO.PDR6.BIT.B0
+#define LED0_ON				LED0 = 0
+#define LED0_OF				LED0 = 1
+
+#define LED1				IO.PDR6.BIT.B1
+#define LED1_ON				LED1 = 0
+#define LED1_OF				LED1 = 1
+
+#define LED2				IO.PDR6.BIT.B2
+#define LED2_ON				LED2 = 0
+#define LED2_OF				LED2 = 1
+
 static unsigned char leds;
 /*---------------------------------------------------------------------------*/
 void
 leds_arch_init(void)
 {
   leds = 0;
+  LED0_OF;
+  LED1_OF;
+  LED2_OF;
 }
 /*---------------------------------------------------------------------------*/
 unsigned char
@@ -56,6 +73,11 @@ leds_arch_get(void)
 void
 leds_arch_set(unsigned char l)
 {
-  leds = l;
+  if (l & LEDS_GREEN) {LED0_ON; leds |= LEDS_GREEN;}
+  else {LED0_OF; leds &= ~LEDS_GREEN;}
+  if (l & LEDS_YELLOW) {LED1_ON; leds |= LEDS_YELLOW;}
+  else {LED1_OF; leds &= ~LEDS_YELLOW;}
+  if (l & LEDS_RED) {LED2_ON; leds |= LEDS_RED;}
+  else {LED2_OF; leds &= ~LEDS_RED;}
 }
 /*---------------------------------------------------------------------------*/
