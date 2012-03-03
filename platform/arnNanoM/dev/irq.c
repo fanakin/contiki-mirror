ca56796b8b3123e40b401123438fef4238b5511b
Fabio Giovagnini <fabio.giovagnini@aurion-tech.com>
2012-03-03 15:57:15 +0100

    new setting for lowlevel init

--- platform/arnNanoM/dev/irq.c
+++ platform/arnNanoM/dev/irq.c
@@ -12,32 +12,31 @@ WDT.TCSRWD.BYTE = 0x10;
 //END
 
 // settaggio porte non usate come output e basse
-IO.PCR1 = (BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT0); // P11 commegato per eventuale PWM
-IO.PDR1.BYTE &= ~0xFD;
+IO.PCR1 = (BIT2 | BIT0); // P10 wismo ON. P12 wismo RST
+IO.PDR1.BYTE |= 0x05;
 
 IO.PCR2 = (BIT4 | BIT3 | BIT0);
-IO.PDR2.BYTE &= ~0x19;
+IO.PDR2.BYTE |= 0x19;
 
-IO.PCR3 = (BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+IO.PCR3 = (BIT2 | BIT1 | BIT0); // eeprom E0,E1,E2 signals
 IO.PDR3.BYTE = 0x00;
 
-IO.PCR5 = (BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
-IO.PDR5.BYTE &= ~0x3F;
+//IO.PCR5 = (BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+//IO.PDR5.BYTE &= ~0x3F;
 
 // P60 Relay0
 // P61 Relay1
 // P62 RelayH
 // P66 TXCT-
-IO.PCR6 = (BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
-IO.PDR6.BYTE &= ~0x38;
-IO.PDR6.BYTE |= 0x47; // TXCT Alto
+IO.PCR6 = (BIT0);
+IO.PDR6.BYTE |= 0x01;
 
 IO.PCR7 = (BIT6 | BIT5 | BIT4);
 IO.PDR7.BYTE &= ~0x70;
 
 // P86 usata come out per notWC signal della m24c01
-IO.PCR8 = (BIT7 | BIT6);
-IO.PDR8.BYTE &= ~0x80;
+IO.PCR8 = (BIT6);
+IO.PDR8.BYTE = 0x00;
 
 }
 
